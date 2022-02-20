53dc6bc13f3a7a517fb56655b102e9b0a15577cd
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2022-02-20 21:53:11 +0100

    Update update.py

--- src/update.py
+++ src/update.py
@@ -1,6 +1,6 @@
 from tkinter import Tk as tk
 from tkinter import Label, messagebox
-import requests
+import requests, os
 
 window = tk()
 window.title("Update")
@@ -12,7 +12,17 @@ Label(window, text="", bg="#bebebe").pack()
 Label(window, text="Looking for updates...", bg="#bebebe").pack()
 req = requests.get("https://raw.githubusercontent.com/HexyeDEV/EzPython/main/version.json").json()
 
+current_version = open("version.txt", "r").read()
+f = open("version.txt", "w")
+
 if req['version'] != current_version:
     messagebox.showinfo("Update", "Update available, Installing it Now")
+    f.write(req['version'])
     new_file = requests.get("https://raw.githubusercontent.com/HexyeDEV/EzPython/main/src/EzPython.py").text
     open("EzPython.py", 'w').write(new_file)
+    messagebox.showinfo("Update", "Update installed")
+    exit()
+else:
+    messagebox.showinfo("Update", "No updates available")
+    os.system("python3 EzPython.py")
+    exit()
