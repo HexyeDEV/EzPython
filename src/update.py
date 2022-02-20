0979fa86757b7099a1890f9cfc1e295218316910
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2022-02-20 21:43:57 +0100

    Add files via upload

---
+++ src/update.py
@@ -0,0 +1,20 @@
+from tkinter import Tk as tk
+from tkinter import Label, messagebox
+import requests
+
+current_version = "0.0.1"
+
+window = tk()
+window.title("Update")
+window.geometry("500x500")
+window.resizable(False, False)
+window.configure(background="#bebebe")
+Label(window, text="Update", font=("Arial", 20), bg="#bebebe").pack()
+Label(window, text="", bg="#bebebe").pack()
+Label(window, text="Looking for updates...", bg="#bebebe").pack()
+req = requests.get("https://raw.githubusercontent.com/HexyeDev/EzPython/master/version.json").json()
+
+if req['version'] != current_version:
+    messagebox.showinfo("Update", "Update available, Installing it Now")
+    new_file = requests.get("https://raw.githubusercontent.com/HexyeDev/EzPython/master/EzPython.py").text
+    open("EzPython.py", 'w').write(new_file)
